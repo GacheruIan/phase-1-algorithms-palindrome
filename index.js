9f0edbaa078fa6ad831633ac7f4a221151bd8bed
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-08-26 16:49:21 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ let palindrome = word.split("").reverse().join("");
 let resultPalindrome = palindrome === word ? true : false;
 return resultPalindrome;
 
+
+
 }
 /* 
   Add your pseudocode here
+  data input === palindrome text
+  if word takes same both backwards and same === true
+  return input
 */
 
 /*
   Add written explanation of your solution here
+  this code takes in a word as an input
+  word example === madam
+  the word should be spelt the same both forward and backward to achieve the result.
 */
 
 // You can run `node index.js` to view these console logs
